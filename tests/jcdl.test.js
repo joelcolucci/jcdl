10fbbef8f0757ab35930cdd2f7856ea3c6b2bee7
Joel Colucci <joelcolucci@gmail.com>
2016-09-26 07:12:02 -0400

    tests: add unittest skeleton for getDownloadLink method

--- tests/jcdl.test.js
+++ tests/jcdl.test.js
@@ -1,7 +1,30 @@
 describe('jcdl', function() {
 
-    it('should say Hello to the World', function() {
-        expect(greet('World')).toEqual('Hello, World!');
+
+    describe('getDownloadLink', function() {
+
+        it('return a jQuery DOM object', function() {
+            var downloadLink = $.jcdl.getDownloadLink();
+
+            var isJqueryObject = downloadLink instanceof jQuery;
+
+            expect(isJqueryObject).toBe(true);
+        });
+
+        it('should have a download attribute', function() {
+
+        });
+
+        it('should have data uri href', function() {
+
+        });
+
     });
+
+
+    describe('_transformToCsv', function() {
+        // Wrapper around jQuery-csv call fromObjects method
+    });
+
 
 });
