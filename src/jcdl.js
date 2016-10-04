87bd604561a7c1eee4490eab9c7740bc351a3861
Joel Colucci <joelcolucci@gmail.com>
2016-10-04 06:49:00 -0400

    style: clean up formatting based on eslint recos

--- src/jcdl.js
+++ src/jcdl.js
@@ -1,89 +1,87 @@
 /**
- * 
+ *
  * jcdl - (jQuery plugin)
- * 
+ *
  * This document is licensed as free software under the terms of the
  * MIT License: http://www.opensource.org/licenses/mit-license.php
- * 
+ *
  * Copyright (c) 2016 Joel Colucci
- * 
+ *
  */
 
 (function($) {
-    'use strict'
+  'use strict';
 
-    $.jcdl = {
+  $.jcdl = {
 
-        /**
-         * 
-         * getDownloadLink
-         * @param {array} data
-         * 
-         */
-    
-        getDownloadLink: function(data) {
-            if (data == null) {
-                throw new TypeError("Argument 'data' must be defined");
-            }
+    /**
+     *
+     * getDownloadLink
+     * @param {array} data
+     *
+     */
 
-            var csvStr = $.jcdl._transformToCsv(data);
-            var encodedCsvStr = $.jcdl._encodeString(csvStr);
+    getDownloadLink: function(data) {
+      if (data == null) {
+        throw new TypeError("Argument 'data' must be defined");
+      }
 
-            var dataUri = $.jcdl._createDataUriHref(encodedCsvStr);
+      var csvStr = $.jcdl._transformToCsv(data);
+      var encodedCsvStr = $.jcdl._encodeString(csvStr);
 
-            var $downloadLink = $('<a>Download</a>');
-            $downloadLink.attr('href', dataUri);
-            $downloadLink.attr('download', 'download');
+      var dataUri = $.jcdl._createDataUriHref(encodedCsvStr);
 
-            return $downloadLink;
-        },
+      var $downloadLink = $('<a>Download</a>');
+      $downloadLink.attr('href', dataUri);
+      $downloadLink.attr('download', 'download');
 
-
-        /**
-         * 
-         * _transformToCsv
-         * @param {array} data - array of objects
-         * 
-         */
-
-        _transformToCsv: function(data) {
-            var flattenedData = flatten2.flattenMany(data);
-
-            return $.csv.fromObjects(flattenedData);
-        },
+      return $downloadLink;
+    },
 
 
-        /**
-         * 
-         * _encodeString
-         * @param {string} string
-         * @return {string}
-         * 
-         */
+    /**
+     *
+     * _transformToCsv
+     * @param {array} data - array of objects
+     *
+     */
 
-        _encodeString: function(str) {
-            return encodeURIComponent(str);
-        },
+    _transformToCsv: function(data) {
+      var flattenedData = flatten2.flattenMany(data);
 
-
-        /**
-         * 
-         * _createDataUriHref
-         * @param {string} string - encoded string of data
-         * @return {string}
-         * 
-         */
-
-        _createDataUriHref: function(str) {
-            return [
-                'data:',
-                'text/csv;',
-                'charset=utf-8,',
-                str  
-            ].join('');
-        }, 
-
-    };
+      return $.csv.fromObjects(flattenedData);
+    },
 
 
+    /**
+     *
+     * _encodeString
+     * @param {string} string
+     * @return {string}
+     *
+     */
+
+    _encodeString: function(str) {
+      return encodeURIComponent(str);
+    },
+
+
+    /**
+     *
+     * _createDataUriHref
+     * @param {string} string - encoded string of data
+     * @return {string}
+     *
+     */
+
+    _createDataUriHref: function(str) {
+      return [
+        'data:',
+        'text/csv;',
+        'charset=utf-8,',
+        str
+      ].join('');
+    }
+
+  };
 })(jQuery);
