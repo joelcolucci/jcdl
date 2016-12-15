1e20c308b062be97a04b0812123c080c761338f8
Joel Colucci <joelcolucci@gmail.com>
2016-12-14 21:43:12 -0500

    feat: run latest dist build

---
+++ dist/jcdl.debug.js
@@ -0,0 +1,81 @@
+'use strict';
+
+/**
+ *
+ * jcdl - (jQuery plugin)
+ *
+ * This document is licensed as free software under the terms of the
+ * MIT License: http://www.opensource.org/licenses/mit-license.php
+ *
+ * Copyright (c) 2016 Joel Colucci
+ *
+ */
+
+(function ($) {
+  'use strict';
+
+  $.jcdl = {
+
+    /**
+     *
+     * getDownloadLink
+     * @param {array} data
+     *
+     */
+
+    getDownloadLink: function getDownloadLink(data) {
+      if (data == null) {
+        throw new TypeError("Argument 'data' must be defined");
+      }
+
+      var csvStr = $.jcdl._transformToCsv(data);
+      var encodedCsvStr = $.jcdl._encodeString(csvStr);
+
+      var dataUri = $.jcdl._createDataUriHref(encodedCsvStr);
+
+      var $downloadLink = $('<a>Download</a>');
+      $downloadLink.attr('href', dataUri);
+      $downloadLink.attr('download', 'download');
+
+      return $downloadLink;
+    },
+
+    /**
+     *
+     * _transformToCsv
+     * @param {array} data - array of objects
+     *
+     */
+
+    _transformToCsv: function _transformToCsv(data) {
+      var flattenedData = flatten2.flattenMany(data);
+
+      return $.csv.fromObjects(flattenedData);
+    },
+
+    /**
+     *
+     * _encodeString
+     * @param {string} string
+     * @return {string}
+     *
+     */
+
+    _encodeString: function _encodeString(str) {
+      return encodeURIComponent(str);
+    },
+
+    /**
+     *
+     * _createDataUriHref
+     * @param {string} string - encoded string of data
+     * @return {string}
+     *
+     */
+
+    _createDataUriHref: function _createDataUriHref(str) {
+      return ['data:', 'text/csv;', 'charset=utf-8,', str].join('');
+    }
+
+  };
+})(jQuery);
